b16f888bc986f467410a9ff52e19acdfec49fd69
zhu <parkjw573@naver.com>
2023-03-15 13:16:11 +0900

    Initial commit

---
+++ src/main/java/zerobase/weather/error/InvalidDateException.java
@@ -0,0 +1,8 @@
+package zerobase.weather.error;
+
+public class InvalidDateException extends RuntimeException{
+    private static final String message = "너무 과거 혹은 미래의 날짜입니다.";
+    public InvalidDateException() {
+        super(message);
+    }
+}
---
+++ src/test/java/zerobase/weather/JdbcMemoRepositoryTest.java
@@ -0,0 +1,42 @@
+package zerobase.weather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import zerobase.weather.domain.Memo;
+import zerobase.weather.repository.JdbcMemoRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+
+@SpringBootTest
+@Transactional
+public class JdbcMemoRepositoryTest {
+    @Autowired
+    JdbcMemoRepository jdbcMemoRepository;
+
+    @Test
+    void insertMemoTest() {
+        // given
+        Memo newMemo = new Memo(2, "insert memo test");
+
+        // when
+        jdbcMemoRepository.save(newMemo);
+
+        // then
+        Optional<Memo> result = jdbcMemoRepository.findById(2);
+        assertEquals(result.get().getText(), "insert memo test");
+    }
+
+    @Test
+    void finalAllMemoTest() {
+        List<Memo> memoList = jdbcMemoRepository.findALl();
+        System.out.println(memoList);
+        assertNotNull(memoList);
+    }
+}
